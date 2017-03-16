9e7856d3896e4f023576a0554448016c9581ae65
Lawrence Williams <lawrence@pushentertainment.com>
2017-03-16 07:36:43 +0000

    title change

--- resources/views/atm.blade.php
+++ resources/views/atm.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-        <title>Laravel</title>
+        <title>Howlett Playground</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
