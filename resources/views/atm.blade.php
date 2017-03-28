7499e76bdfcbf63b0d443f92d410f2f83aff389b
Lawrence Williams <lawrence@pushentertainment.com>
2017-03-28 20:37:11 +0100

    add homepage and sensible title

--- resources/views/atm.blade.php
+++ resources/views/atm.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-        <title>Howlett Playground</title>
+        <title>Open Banking API Playground ATM Demo</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
